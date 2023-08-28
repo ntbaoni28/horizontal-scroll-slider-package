dce9b6f4fbeb535db038aa17563e6cf1ca7ab168
Ni Nguyen <ni@vietswiss.com>
2023-08-28 17:37:19 +0700

    Initial pages commit

--- README.md
+++
@@ -1,1 +0,0 @@
-# horizontal-scroll-slider-package
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,48 @@
-import HorizontalScrollSlider from "./components/HorizontalScrollSlider";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import HorizontalScrollSlider from "horizontal-scroll-slider";
 
-export default HorizontalScrollSlider;
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+root.render(
+  <React.StrictMode>
+    <HorizontalScrollSlider
+      scrollHeight="500vh"
+      className="horizontal-scroll-slider"
+      header={<h2>Horizontal Scroll Slider Demo 1</h2>}
+    >
+      <div className="slides">
+        {Array.from(Array(15)).map((_, index) => (
+          <div key={`slide-${index}`} className="slide">
+            {index + 1}
+          </div>
+        ))}
+      </div>
+    </HorizontalScrollSlider>
+    <HorizontalScrollSlider
+      scrollHeight="300vh"
+      className="horizontal-scroll-slider"
+      header={<h2>Horizontal Scroll Slider Demo 2</h2>}
+    >
+      <div className="slides">
+      <div className="text">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam facere
+        quia, deleniti doloremque corrupti vero nam minima illo saepe eius
+        laudantium assumenda commodi delectus molestias debitis perspiciatis
+        nostrum excepturi accusamus! Lorem ipsum dolor, sit amet consectetur
+        adipisicing elit. Aliquam facere quia, deleniti doloremque corrupti vero
+        nam minima illo saepe eius laudantium assumenda commodi delectus
+        molestias debitis perspiciatis nostrum excepturi accusamus! Lorem ipsum
+        dolor, sit amet consectetur adipisicing elit. Aliquam facere quia,
+        deleniti doloremque corrupti vero nam minima illo saepe eius laudantium
+        assumenda commodi delectus molestias debitis perspiciatis nostrum
+        excepturi accusamus! Lorem ipsum dolor, sit amet consectetur adipisicing
+        elit. Aliquam facere quia, deleniti doloremque corrupti vero nam minima
+        illo saepe eius laudantium assumenda commodi delectus molestias debitis
+        perspiciatis nostrum excepturi accusamus!
+      </div></div>
+    </HorizontalScrollSlider>
+  </React.StrictMode>
+);
